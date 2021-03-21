21eaab0f9d883875085f41e9e16e5c065f56c07c
Tenchia <suslik22881@gmail.com>
2021-03-22 01:49:35 +0200

    надо
    дополнение

--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,33 @@
-//============================================================================
-// Name        : test.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+long double fact(int N)
+{
+    if(N < 0) // если пользователь ввел отрицательное число
+        return 0; // возвращаем ноль
+    if (N == 0) // если пользователь ввел ноль,
+        return 1; // возвращаем факториал от нуля - не удивляетесь, но это 1 =)
+    else // Во всех остальных случаях
+        return N * fact(N - 1); // делаем рекурсию.
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	int amnt;
+	double i = 1;
+	double x, f;
+	double sum = 0;
+	setlocale(0, "");
+
+	cout << "Введите кол-во итераций и значение х:" << endl;
+	cin >> amnt >> x;
+
+	do {
+		f = i * cos(pow(i + x, 2))/fact(3*i + x);
+		sum = sum + f;
+		i++;
+	} while (i < amnt);
+
+	cout << "Значение суммы " << amnt << " итераций: " << sum;
 }
